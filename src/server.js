9f3c98895b8468fd507c9bb062a2b5911637a526
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-06-14 15:57:58 -0300

    :art: built the html struture and styled

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const PORT = process.env.PORT || 3000
 
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.get('/', (req, res) => {
-    return res.send('HELLO')
+app.get('*', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 
